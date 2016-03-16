0a95186a003f8240e7fe2183a4d7db8d1c7af2dd
TT_dashen <ltt143@126.com>
2016-03-16 18:12:45 +0800

    first

---
+++ TTTimerImageDemo/TTTimerImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTimerImageDemo
+//
+//  Created by 梁腾 on 16/3/16.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTTimerImageDemo/TTTimerImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTTimerImageDemo
+//
+//  Created by 梁腾 on 16/3/16.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
